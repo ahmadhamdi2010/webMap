8358bc9541ab1c7aadb2aaf7db5c8ef32e1c4dce
ahmadhamdi2010 <ahmad2013hamdi@gmail.com>
2023-04-24 22:52:41 +0200

    Added the volcanoes by a features group, added another FG for a geoJson layer that colors itself based on the population

--- map1.py
+++ map1.py
@@ -9,7 +9,10 @@ dLat = list(data["LAT"])
 dLon = list(data["LON"])
 dLev = list(data["ELEV"])
 
-myFeatureGroup = folium.FeatureGroup(name="My Map")
+
+
+
+FGV = folium.FeatureGroup(name="Volcanos")
 
 for lt, ln, el in zip(dLat, dLon, dLev):
 
@@ -20,9 +23,13 @@ for lt, ln, el in zip(dLat, dLon, dLev):
     else :
         pcolor = 'green'
         
-    myFeatureGroup.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon(color=pcolor) ))
+    FGV.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon(color=pcolor) ))
 
-myMap.add_child(myFeatureGroup)
+myMap.add_child(FGV)
+FGP = folium.FeatureGroup(name="Population")
+FGP.add_child(folium.GeoJson(data=open("world.json", 'r', encoding='utf-8-sig').read(), style_function=lambda x : {'fillColor':'green' if x['properties']['POP2005']<10000000 else 'orange' if x['properties']['POP2005'] >10000000 else 'red '}))
 
+myMap.add_child(FGP)
+myMap.add_child(folium.LayerControl())
 #myMap.add_child(folium.Marker(location=[38.2,-99.1],popup=" HI am a market",icon=folium.Icon(color='green') ))
 myMap.save("Map1.html")
