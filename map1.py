048fdec43cc212464a3ef89def5a824c2d72bae1
ahmadhamdi2010 <ahmad2013hamdi@gmail.com>
2023-04-24 22:04:16 +0200

    added markers on Volcano locations, colored the markers according to the elevation of each volcano

--- map1.py
+++ map1.py
@@ -1,9 +1,27 @@
 import folium
+import pandas
+
 
 myMap = folium.Map(location=[38,-100],zoom_start=6, TileLayer="Mapbox Bright")
+data = pandas.read_csv("Volcanoes_USA.txt")
+
+dLat = list(data["LAT"])
+dLon = list(data["LON"])
+dLev = list(data["ELEV"])
 
 myFeatureGroup = folium.FeatureGroup(name="My Map")
-myFeatureGroup.add_child(folium.Marker(location=[38.2,-99.1],popup=" HI am a market",icon=folium.Icon(color='green') ))
+
+for lt, ln, el in zip(dLat, dLon, dLev):
+
+    if el > 3000 :
+        pcolor = 'red'
+    elif el > 2000 :
+        pcolor = 'pink'
+    else :
+        pcolor = 'green'
+        
+    myFeatureGroup.add_child(folium.Marker(location=[lt,ln],popup=str(el)+" m",icon=folium.Icon(color=pcolor) ))
+
 myMap.add_child(myFeatureGroup)
 
 #myMap.add_child(folium.Marker(location=[38.2,-99.1],popup=" HI am a market",icon=folium.Icon(color='green') ))
