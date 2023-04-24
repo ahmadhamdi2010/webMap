a1474a9bce81c2908c251479264d167f142ac468
ahmadhamdi2010 <ahmad2013hamdi@gmail.com>
2023-04-24 21:26:53 +0200

    added a marker with features group

--- map1.py
+++ map1.py
@@ -1,4 +1,10 @@
 import folium
 
 myMap = folium.Map(location=[38,-100],zoom_start=6, TileLayer="Mapbox Bright")
+
+myFeatureGroup = folium.FeatureGroup(name="My Map")
+myFeatureGroup.add_child(folium.Marker(location=[38.2,-99.1],popup=" HI am a market",icon=folium.Icon(color='green') ))
+myMap.add_child(myFeatureGroup)
+
+#myMap.add_child(folium.Marker(location=[38.2,-99.1],popup=" HI am a market",icon=folium.Icon(color='green') ))
 myMap.save("Map1.html")
